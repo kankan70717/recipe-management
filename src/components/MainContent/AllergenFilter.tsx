2e9d66ed15227238a4a7afce2da06b83e5c9b185
Kanta Nagai <93016258+kankan70717@users.noreply.github.com>
2025-07-24 16:13:25 -0700

    edit

--- src/components/MainContent/AllergenFilter.tsx
+++ src/components/MainContent/AllergenFilter.tsx
@@ -1,27 +1,42 @@
+import { useEffect, useState } from "react";
 import { useSettings } from "../../context/SettingsContext";
 
 export default function AllergenFilter() {
 	const settings = useSettings();
+	const [info, setInfo] = useState(null);
+
+	useEffect(() => {
+		setInfo(settings?.allergen)
+	}, [settings]);
 
 	return (
 		<div className="py-5 px-20 w-full bg-gray-100 overflow-scroll">
 			<div className="p-5 rounded-xl shadow-xl bg-white flex gap-5">
 				<div className="">
 					<h2 className="text">Filter by</h2>
-					<ul className="list-none">
-						<li className="capitalize">
+					<ul className="list-none capitalize">
+						<li onClick={() => setInfo(settings?.allergen)}>
 							allergens
+						</li>
+						<li onClick={() => setInfo(settings?.dish.category)}>
+							category
+						</li>
+						<li onClick={() => setInfo(settings?.dish.tag)}>
+							tag
 						</li>
 					</ul>
 				</div>
 				<div>
 					<ul className="list-none">
-						<li>
-							nuts
-						</li>
-						<li>
-							almond
-						</li>
+						{
+							info?.map((item, index) => {
+								if (typeof item === 'object') {
+									return (<li key={index}>{item.category}</li>);
+								} else {
+									return (<li key={index}>{JSON.stringify(item)}</li>);
+								}
+							})
+						}
 					</ul>
 				</div>
 			</div>
